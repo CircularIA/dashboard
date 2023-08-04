2559fbecd6bb08fa02b1b67d48e04d12d41e1247
javier-tralma <javier.tralma@sansano.usm.cl>
2023-08-04 02:34:09 -0400

    feat: route changes, unfinished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Login from './pages/Login'
-import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
+          <Route path='/' element={<Dashboard />} />
         </Routes>
       </Router>
     </>
