9ba7b0381bb13f7a82806eae3f0eb5a31297334d
javier-tralma <javier.tralma@sansano.usm.cl>
2023-08-12 14:03:04 -0400

    feat: Init valorization component (uncomplete)

---
+++ src/components/Valorization.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Valorization = () => {
+    return (
+        <>
+            {/* <div className='mx-8'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing enim eu turpis egestas pretium aenean pharetra. Ullamcorper malesuada proin libero nunc consequat interdum varius sit. Sapien pellentesque habitant morbi tristique senectus et. Eget duis at tellus at urna. Ultrices tincidunt arcu non sodales neque sodales ut. Cras sed felis eget velit aliquet sagittis id. Mauris sit amet massa vitae tortor condimentum. Mauris ultrices eros in cursus turpis massa tincidunt dui ut. Vel facilisis volutpat est velit egestas dui id. Mi bibendum neque egestas congue quisque egestas. Aliquam vestibulum morbi blandit cursus risus at ultrices. Volutpat sed cras ornare arcu dui vivamus arcu. Auctor eu augue ut lectus arcu bibendum.
+
+                Diam volutpat commodo sed egestas egestas. Sed sed risus pretium quam vulputate dignissim. Lectus arcu bibendum at varius. Elit at imperdiet dui accumsan sit amet. Quis lectus nulla at volutpat. Scelerisque felis imperdiet proin fermentum leo vel. Vestibulum lectus mauris ultrices eros in. Nunc mattis enim ut tellus elementum sagittis vitae et. Est ultricies integer quis auctor. Auctor urna nunc id cursus metus aliquam. Scelerisque in dictum non consectetur a erat nam at. Vel fringilla est ullamcorper eget nulla facilisi etiam. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Mi in nulla posuere sollicitudin aliquam. Eu consequat ac felis donec et odio pellentesque diam volutpat. Turpis massa tincidunt dui ut ornare. In eu mi bibendum neque egestas congue quisque egestas.
+            </div> */}
+        </>
+    );
+};
+
+export default Valorization;
