ef0e9257ffefc4de667a1fe9e1e4b1c3a79801ae
javier-tralma <javier.tralma@sansano.usm.cl>
2023-08-29 22:07:44 -0400

    feat: theme reducer for panel types

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authSlice';
+import themeReducer from './themeReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  theme: themeReducer,
   // Otros reducers aquí si los tienes
 });
 
