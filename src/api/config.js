f32378322294b4006326657b2f29f38557970fca
javier-tralma <javier.tralma@sansano.usm.cl>
2024-01-05 17:24:28 -0300

    update: routes in config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,26 +1,34 @@
 //Have to use env variable to get the api url
 
 export const userRoutes = {
-    getUser: 'http://localhost:5000/api/users',
+    getUser: 'http://localhost:5000/user',
+}
+
+export const authRoutes = {
+    loginRoute: 'http://localhost:5000/auth',
+    forgetPasswordRoute: 'http://localhost:5000/auth/forgetPassword'
 }
 
 export const branchRoutes = {
-    getBranches: 'http://localhost:5000/api/branch',
-    getIndicators: 'http://localhost:5000/api/branch/indicators/',
+    getBranches: 'http://localhost:5000/branch',
+    getIndicators: 'http://localhost:5000/branch/indicators/',
 }
 
 export const companyRoutes = {
-    getCompanyInfo: 'http://localhost:5000/api/company',
+    getCompanyInfo: 'http://localhost:5000/company',
 }
 
 export const indicatorRoutes = {
-    getIndicators: 'http://localhost:5000/api/indicator',
-    registerIndicators: 'http://localhost:5000/api/indicator/register',
-    getIndicatorValues: 'http://localhost:5000/api/indicator/values', //:branch/:indicator/:year/:month?
+    getIndicators: 'http://localhost:5000/indicator',
+    registerIndicators: 'http://localhost:5000/indicator/register',
+    getIndicatorValues: 'http://localhost:5000/indicator/values'
 }
 
 export const inputDatsRoutes = {
-    getInputDats: 'http://localhost:5000/api/inputDat',
-    getInputDatsByIndicator: 'http://localhost:5000/api/inputDat/byIndicator/', //:branch/:indicator
-    registerInputDats: 'http://localhost:5000/api/inputDat'
+    getInputDats: 'http://localhost:5000/inputDat',
+    registerInputDats: 'http://localhost:5000/inputDat',
+    getInputDatsByIndicator: 'http://localhost:5000/inputDat/byIndicator',
+    updateInputDats: 'http://localhost:5000/inputDat/update',
+    registerInputDatsMany: 'http://localhost:5000/inputDat/many/',
+    getIndicatorsByBranch: 'http://localhost:5000/indicator'
 }
